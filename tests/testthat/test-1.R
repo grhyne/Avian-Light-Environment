295a73de962c925339315fa4e22847706a84de7d
grhyne <grhyne1@lsu.edu>
2020-12-01 13:41:59 -0600

    Add first test

--- tests/testthat/test-1.R
+++ tests/testthat/test-1.R
@@ -1,5 +1,11 @@
 context("")
 
 test_that("daylightData", {
-  expect_equal(ncol(pdHFSA_inc), ncol(pdMGSA_inc))
+  expect_is(daylightData, "data.frame")
+  expect_equal(ncol(daylightData), 3)
+})
+
+test_that("daylightData2", {
+  expect_is(daylightData2, "data.frame")
+  expect_equal(ncol(daylightData2), 3)
 })
